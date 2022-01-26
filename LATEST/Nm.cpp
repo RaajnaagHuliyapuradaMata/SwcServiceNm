90806ab58a489c60c491ca525df49037854a52b2
root <nagaraja.hm.0001@gmail.com>
2022-01-26 10:24:00 +0100

    Update: submodule Nm

--- LATEST/Nm.cpp
+++ LATEST/Nm.cpp
@@ -5,93 +5,93 @@ class_Nm Nm;
 FUNC(void, NM_CODE) class_Nm::InitFunction(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::PassiveStartUp(void){
+FUNC(void, NM_CODE) class_Nm::PassiveStartUp(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::NetworkRequest(void){
+FUNC(void, NM_CODE) class_Nm::NetworkRequest(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::NetworkRelease(void){
+FUNC(void, NM_CODE) class_Nm::NetworkRelease(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::DisableCommunication(void){
+FUNC(void, NM_CODE) class_Nm::DisableCommunication(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::EnableCommunication(void){
+FUNC(void, NM_CODE) class_Nm::EnableCommunication(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::GetPduData(void){
+FUNC(void, NM_CODE) class_Nm::GetPduData(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::GetUserData(void){
+FUNC(void, NM_CODE) class_Nm::GetUserData(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::SetUserData(void){
+FUNC(void, NM_CODE) class_Nm::SetUserData(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::RepeateMessageRequest(void){
+FUNC(void, NM_CODE) class_Nm::RepeateMessageRequest(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::GetNodeIdentifier(void){
+FUNC(void, NM_CODE) class_Nm::GetNodeIdentifier(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::GetLocalNodeIdentifier(void){
+FUNC(void, NM_CODE) class_Nm::GetLocalNodeIdentifier(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::CheckRemoteSleepIndication(void){
+FUNC(void, NM_CODE) class_Nm::CheckRemoteSleepIndication(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::GetState(void){
+FUNC(void, NM_CODE) class_Nm::GetState(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::GetVersionInfo(void){
+FUNC(void, NM_CODE) class_Nm::GetVersionInfo(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::CbNetworkStartIndication(void){
+FUNC(void, NM_CODE) class_Nm::CbNetworkStartIndication(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::CbNetworkMode(void){
+FUNC(void, NM_CODE) class_Nm::CbNetworkMode(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::CbBusSleepMode(void){
+FUNC(void, NM_CODE) class_Nm::CbBusSleepMode(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::CbPrepareBusSleepMode(void){
+FUNC(void, NM_CODE) class_Nm::CbPrepareBusSleepMode(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::CbRemoteSleepIndication(void){
+FUNC(void, NM_CODE) class_Nm::CbRemoteSleepIndication(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::CbRemoteSleepCancellation(void){
+FUNC(void, NM_CODE) class_Nm::CbRemoteSleepCancellation(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::CbSynchronizationPoint(void){
+FUNC(void, NM_CODE) class_Nm::CbSynchronizationPoint(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::CbCoordReadyToSleepIndication(void){
+FUNC(void, NM_CODE) class_Nm::CbCoordReadyToSleepIndication(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::CbCoordReadyToSleepCancellation(void){
+FUNC(void, NM_CODE) class_Nm::CbCoordReadyToSleepCancellation(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::CbPduRxIndication(void){
+FUNC(void, NM_CODE) class_Nm::CbPduRxIndication(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::CbStateChangeNotification(void){
+FUNC(void, NM_CODE) class_Nm::CbStateChangeNotification(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::CbRepeateMessageIndication(void){
+FUNC(void, NM_CODE) class_Nm::CbRepeateMessageIndication(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::CbTxTimeoutException(void){
+FUNC(void, NM_CODE) class_Nm::CbTxTimeoutException(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::CbCarWakeUpIndication(void){
+FUNC(void, NM_CODE) class_Nm::CbCarWakeUpIndication(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::MainFunction(void){
+FUNC(void, NM_CODE) class_Nm::MainFunction(void){
 }
 
-FUNC(void, NM_CODE) class_Adc::CalloutCarWakeUp(void){
+FUNC(void, NM_CODE) class_Nm::CalloutCarWakeUp(void){
 }
 
