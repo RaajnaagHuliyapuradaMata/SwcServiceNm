fab7123ac442989deb31d148ec73f55991890602
root <nagaraja.hm.0001@gmail.com>
2022-01-26 08:57:15 +0100

    Update: submodule Nm

--- LATEST/Nm.cpp
+++ LATEST/Nm.cpp
@@ -5,3 +5,93 @@ class_Nm Nm;
 FUNC(void, NM_CODE) class_Nm::InitFunction(void){
 }
 
+FUNC(void, NM_CODE) class_Adc::PassiveStartUp(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::NetworkRequest(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::NetworkRelease(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::DisableCommunication(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::EnableCommunication(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::GetPduData(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::GetUserData(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::SetUserData(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::RepeateMessageRequest(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::GetNodeIdentifier(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::GetLocalNodeIdentifier(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::CheckRemoteSleepIndication(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::GetState(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::GetVersionInfo(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::CbNetworkStartIndication(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::CbNetworkMode(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::CbBusSleepMode(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::CbPrepareBusSleepMode(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::CbRemoteSleepIndication(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::CbRemoteSleepCancellation(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::CbSynchronizationPoint(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::CbCoordReadyToSleepIndication(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::CbCoordReadyToSleepCancellation(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::CbPduRxIndication(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::CbStateChangeNotification(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::CbRepeateMessageIndication(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::CbTxTimeoutException(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::CbCarWakeUpIndication(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::MainFunction(void){
+}
+
+FUNC(void, NM_CODE) class_Adc::CalloutCarWakeUp(void){
+}
+
--- LATEST/Nm.h
+++ LATEST/Nm.h
@@ -5,7 +5,37 @@
 
 class class_Nm{
    public:
-      FUNC(void, NM_CODE) InitFunction(void);
+      FUNC(void, NM_CODE) InitFunction                    (void);
+      FUNC(void, NM_CODE) PassiveStartUp                  (void);
+      FUNC(void, NM_CODE) NetworkRequest                  (void);
+      FUNC(void, NM_CODE) NetworkRelease                  (void);
+      FUNC(void, NM_CODE) DisableCommunication            (void);
+      FUNC(void, NM_CODE) EnableCommunication             (void);
+      FUNC(void, NM_CODE) GetPduData                      (void);
+      FUNC(void, NM_CODE) GetUserData                     (void);
+      FUNC(void, NM_CODE) SetUserData                     (void);
+      FUNC(void, NM_CODE) RepeateMessageRequest           (void);
+      FUNC(void, NM_CODE) GetNodeIdentifier               (void);
+      FUNC(void, NM_CODE) GetLocalNodeIdentifier          (void);
+      FUNC(void, NM_CODE) CheckRemoteSleepIndication      (void);
+      FUNC(void, NM_CODE) GetState                        (void);
+      FUNC(void, NM_CODE) GetVersionInfo                  (void);
+      FUNC(void, NM_CODE) CbNetworkStartIndication        (void);
+      FUNC(void, NM_CODE) CbNetworkMode                   (void);
+      FUNC(void, NM_CODE) CbBusSleepMode                  (void);
+      FUNC(void, NM_CODE) CbPrepareBusSleepMode           (void);
+      FUNC(void, NM_CODE) CbRemoteSleepIndication         (void);
+      FUNC(void, NM_CODE) CbRemoteSleepCancellation       (void);
+      FUNC(void, NM_CODE) CbSynchronizationPoint          (void);
+      FUNC(void, NM_CODE) CbCoordReadyToSleepIndication   (void);
+      FUNC(void, NM_CODE) CbCoordReadyToSleepCancellation (void);
+      FUNC(void, NM_CODE) CbPduRxIndication               (void);
+      FUNC(void, NM_CODE) CbStateChangeNotification       (void);
+      FUNC(void, NM_CODE) CbRepeateMessageIndication      (void);
+      FUNC(void, NM_CODE) CbTxTimeoutException            (void);
+      FUNC(void, NM_CODE) CbCarWakeUpIndication           (void);
+      FUNC(void, NM_CODE) MainFunction                    (void);
+      FUNC(void, NM_CODE) CalloutCarWakeUp                (void);
 };
 
 extern class_Nm Nm;
