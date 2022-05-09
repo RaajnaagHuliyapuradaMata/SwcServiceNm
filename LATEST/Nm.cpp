d866818a05b063ffa632c8c41f62670680a584eb
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-09 10:36:42 +0000

    Update: AUTOSAR basic software service module Nm

--- LATEST/Nm.cpp
+++ LATEST/Nm.cpp
@@ -35,6 +35,13 @@ class module_Nm:
       public abstract_module
    ,  public class_Nm_Functionality
 {
+   private:
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+            Std_TypeReturn          IsInitDone{E_NOT_OK};
+      const CfgModule_TypeAbstract* lptrCfg{(CfgModule_TypeAbstract*)NULL_PTR};
+
    public:
       module_Nm(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
       }
