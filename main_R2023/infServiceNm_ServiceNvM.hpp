9e6499dbf9ac9bf277b5307430caa58420aba655
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:50:22 +0100

    Update: AUTOSAR basic software service module for generic network manager

---
+++ main_R2023/infServiceNm_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infServiceNm_ServiceNvM.hpp                                       */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTSERVICENM_AR_RELEASE_VERSION_MAJOR                                4
+#define CONSTSERVICENM_AR_RELEASE_VERSION_MINOR                                3
+
+#define Const_DefaultInit_ConstServiceNm                                                                             \
+   ,  {CONSTSERVICENM_AR_RELEASE_VERSION_MAJOR, CONSTSERVICENM_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
